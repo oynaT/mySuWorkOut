15a7bd8b638180418c2d70ee10be4c8ddb71bc0f
tanyo <tanyo@zencharts.com>
2022-12-02 19:35:20 +0200

    some changes

--- 33. Exam Preparation/03. Plant Discovery.js
+++ 33. Exam Preparation/03. Plant Discovery.js
@@ -1,11 +1,3 @@
-//03. Plant Discovery
-//https://pastebin.com/KJK34a8J
-//https://pastebin.com/vkUFLsaf
-//https://pastebin.com/PtasjuY6
-//https://pastebin.com/gNZyEXBS
-//https://pastebin.com/LYZa0aC7
-//https://pastebin.com/A9npbd27
-
 //За сортирането може и така let sorted = b[1].rarity - a[1].rarity || b[1].rating- a[1].rating
 
 //console.table()
